1592675616a838be6539a2ad6620db8de82d5dfc
sunqichao <sunqichaojob@gmail.com>
2013-01-22 06:27:15 +0800

    Initial Commit

---
+++ SQCTableViewCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SQCTableViewCell
+//
+//  Created by sun qichao on 13-1-22.
+//  Copyright (c) 2013年 sun qichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SQCTableViewCell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SQCTableViewCell
+//
+//  Created by sun qichao on 13-1-22.
+//  Copyright (c) 2013年 sun qichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
